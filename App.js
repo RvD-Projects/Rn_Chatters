8e65a5ac49c82a3d1c41a63fa7dc1358ad5d3fd4
Raphael Vigneault Ð <triangle123209@hotmail.com>
2023-05-14 22:15:00 -0400

    socket Base

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { Provider } from "react-redux";
 import { store } from "./src/store/store";
 import RootNavigation from "./src/components/navigators/RootNavigation";
 import AppScreenNotification from "./src/components/AppScreenNotification";
+import SocketIo from "./src/helpers/SocketIo";
+
 
 function App() {
   return (
     <React.StrictMode>
       <Provider store={store}>
+        <SocketIo/>
         <RootNavigation />
         <AppScreenNotification />
       </Provider>
---
+++ src/helpers/SocketIo.js
@@ -0,0 +1,98 @@
+import React from "react";
+import io from "socket.io-client";
+import { updateStateUser } from "../reducers/UserReducer";
+import { connect } from "react-redux";
+import { sendNotification } from "../reducers/NotificationsReducer";
+import { SESSION_SERVICE_URL } from "@env";
+
+const SocketIo = (props) => {
+  const URL = SESSION_SERVICE_URL;
+  const socket = io(URL, { autoConnect: false });
+  socket.auth = { email: "webdevteam@rvdprojects.com", password: "plainText" };
+
+  function onConnect(event, ...args) {
+    console.warn("Socket-Connection:", URL);
+    console.info(event, "\n", args, "\r --------------------------------");
+  }
+
+  function onSession(event, ...args) {
+    console.info("onSession:");
+    console.info(event, "\n", args, "\r --------------------------------");
+
+    props.updateStateUser(event);
+  }
+
+  function onUserConnected(event, ...args) {
+    console.info("onUserConnected:");
+    console.info(event, "\n", args, "\r --------------------------------");
+  }
+
+  function onUserDisconnected(event, ...args) {
+    console.info("onUserDisconnected:");
+    console.info(event, "\n", args, "\r --------------------------------");
+  }
+
+  function onUsers(users, ...args) {
+    console.info("onUsers:");
+
+    users.forEach((user) => {
+      user.self = user.userID === socket.id;
+      //initReactiveProperties(user);
+    });
+    // put the current user first, and then sort by username
+    users = users.sort((a, b) => {
+      if (a.self) return -1;
+      if (b.self) return 1;
+      if (a.username < b.username) return -1;
+      return a.username > b.username ? 1 : 0;
+    });
+
+    console.info(users, "\n", args, "\r --------------------------------");
+  }
+
+  function onPrivateMessage(event, ...args) {
+    console.info("onPrivateMessage:");
+    console.info(event, "\n", args, "\r --------------------------------");
+  }
+
+  function onDisconnect(event, ...args) {
+    console.info("onDisconnect:");
+    console.error(event, "\n", args, "\r --------------------------------");
+  }
+
+  const listeners = [
+    "connect",
+    "session",
+    "userConnected",
+    "onUserDisconnected",
+    "users",
+    "privateMessage",
+    "disconnect",
+  ];
+
+  socket.on("connect", onConnect);
+  socket.on("session", onSession);
+  socket.on("userConnected", onUserConnected);
+  socket.on("userDisconnected", onUserDisconnected);
+  socket.on("users", onUsers);
+  socket.on("privateMessage", onPrivateMessage);
+  socket.on("disconnect", onDisconnect);
+
+  socket.onAny((event, ...args) => {
+    if (!listeners.includes(event)) {
+      console.error("!!!!!!!!!!!!!!!!!! onAny: !!!!!!!!!!!!!!!!!!");
+      console.error(event, "\n", args, "\r --------------------------------");
+    }
+  });
+
+  socket.connect();
+
+  return;
+};
+
+const mapDispatchToProps = {
+  sendNotification,
+  updateStateUser,
+};
+
+export default connect(null, mapDispatchToProps)(SocketIo);
